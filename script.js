36bda7b06cc0bfad8c711242e3a7b4d862ce2a4f
thomas-nelson23 <te.nelson993@gmail.com>
2019-10-09 20:00:44 -0500

    working clook - looks good

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
-var clockTime = new Date();
-var clockSpace = document.getElementById("clock");
+var updateTime = function() {
+  var clockTime = new Date();
+  var clockSpace = document.getElementById("clock");
+  var meridian = "AM";
 
-//clockSpace.innerHTML = clockTime;
+  var hours = clockTime.getHours();
+  var minutes = clockTime.getMinutes();
+  var seconds = clockTime.getSeconds();
 
-var hours = clockTime.getHours();
-if (hours > 12) {
-  hours -= 12;
-}
+  //chooses AM/PM and adjusts from military time
+  if (hours > 11) {
+    meridian = "PM";
+  }
 
-var minutes = clockTime.getMinutes();
-var seconds = clockTime.getSeconds();
+  if (hours > 12) {
+    hours -= 12;
+  }
 
-console.log(minutes, seconds);
+  //adding "0" to 1-digit numbers for spacing
+  if (hours < 10) {
+    hours = "0" + hours;
+  }
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+  if (seconds < 10) {
+    seconds = "0" + seconds;
+  }
+
+  //displays time
+  clockSpace.innerHTML = hours + ":" + minutes + ":" + seconds + " " + meridian;
+};
+
+var oneSecond = 1000;
+setInterval(updateTime, oneSecond);
