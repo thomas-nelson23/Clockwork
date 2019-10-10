01494c66102bfa1cf671b0a02d5bfd590a8a8aee
thomas-nelson23 <te.nelson993@gmail.com>
2019-10-09 19:17:35 -0500

    clock plugged in to grab numbers. interface still not right

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 var clockTime = new Date();
 var clockSpace = document.getElementById("clock");
 
-clockSpace.innerHTML = clockTime;
+//clockSpace.innerHTML = clockTime;
+
+var hours = clockTime.getHours();
+if (hours > 12) {
+  hours -= 12;
+}
+
+var minutes = clockTime.getMinutes();
+var seconds = clockTime.getSeconds();
+
+console.log(minutes, seconds);
